5675efc1c7aeb66b58d3757105d707347533adc8
성열 <csy5501@gmail.com>
2023-07-19 15:22:47 +0900

    0719 update post_share view (email)

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 # from django.http import Http404
+from django.core.mail import send_mail
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 
@@ -46,10 +47,17 @@ def post_detail(request, year, month, day, post):
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
     if request.method == 'POST':
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']}님이 {post.title}을(를) 추천합니다."
+            message = f"{post.title}을(를) {post_url}에서 읽어보세요.\n\n" \
+                      f"{cd['name']}의 의견: {cd['comments']}"
+            send_mail(subject, message, 'csy5501@gmail.com', cd['to'])
+            sent = True
     else:
         form = EmailPostForm()
-    return render(request, 'blog/post/share.html', {'post': post, 'form': form})
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
