28ff887458a260023ebb171ab3f2243e81c50fe4
성열 <csy5501@gmail.com>
2023-07-19 23:04:00 +0900

    update sending email with env

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 # from django.http import Http404
+import os
 from django.core.mail import send_mail
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
--- mysite/settings.py
+++ mysite/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -124,8 +126,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Email
+load_dotenv()
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'csy5501@gmail.com'
-EMAIL_HOST_PASSWORD = '************'
+EMAIL_HOST_USER = os.getenv('email')
+EMAIL_HOST_PASSWORD = os.getenv('password')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
